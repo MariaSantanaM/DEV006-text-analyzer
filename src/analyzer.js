10f50ffc919f0b1236d4120b8c2129eea2f534de
MariaSantanaM <maria.lsm.0306@gmail.com>
2023-08-15 16:27:35 -0400

    https://github.com/MariaSantanaM/DEV006-text-analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,83 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
+
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    //crear una constante para que me arroje el resultado con trim()
+    const stringTrim = text.trim();
+    console.log(stringTrim.length, "textoTrim")
+
+    //contare las palabras del texto usare .split() 
+
+    const getWordCount = stringTrim.split(/\s+/).length;
+    //(/\s+/) expresiones regulares, las expresiones regulares buscan patrones
+
+    return getWordCount;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+    //primero elimino todos los espacios y signos de text
+    const character = text.replace(/[^\w]/g, "")
+
+    // Retornar el recuento de caracteres sin espacios ni signos de puntuación
+    return character.length;
   },
-  getAverageWordLength: (text) => {    
+
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    // Dividir el texto en palabras individuales usando espacios como delimitador
+    const words = text.split(' ');
+    console.log(words)
+
+    // Calcular la suma de las longitudes de todas las palabras
+    const totalLength = words.reduce((acc, word) => acc + word.length, 0);
+
+    // Calcular la longitud media dividiendo la suma por el número total de palabras
+    const averageLength = totalLength / words.length;
+
+    //parseFloat recibe una cadena creada con toFixed con solo dos decimales, y toda esta cadena la pasa a numeros flotantes 
+
+    return parseFloat(averageLength.toFixed(2))    
+
   },
+
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    //buscar otra forma que un numero decimal me lo cuente como uno entero y no cad uno por separado en el conteo final
+    //"Esto no es un número: 41u0003jot" que no me lo tome como numero porque tiene letras
+    const numberCoincidencia = /\b[0-9]+(?:\.[0-9]+)?\b/g;
+    const numbers = text.match(numberCoincidencia);
+    //devuelve un array con todo lo encontrado o coincidencia
+
+    return numbers ? numbers.length : 0;
+    //operador ternario if/else que nos regrese algo = ?
   },
+
+
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numberRegex = /\b[0-9]+(?:\.[0-9]+)?\b/g;
+    // la expresion regular es para encontrar los numeros en el texto 
+    const numbers = text.match(numberRegex);
+    //metodo match para encontrar todas las ocurrencias o coincidencias en el texto
+
+    if (!numbers) {
+      //si no se encuentran numeros en el textro retorna 0
+      return 0;
+    }
+
+    const sum = numbers.reduce((accumulator, currentValue) => {
+      return accumulator + parseFloat(currentValue);
+    }, 0);
+
+    return sum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// primero paso traer todos los li
+const liTodos = document.querySelectorAll("li")
+console.log(liTodos)
+
+//decima prueba
+const textarea = document.querySelector('textarea[name="user input"]');
+const liPalabras = liTodos[0];
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  console.log(textToAnalyze.length, "textoOriginal")
+  const numeroPalabras = analyzer.getWordCount(textToAnalyze);
+  liPalabras.innerHTML = `Palabras: ${numeroPalabras}`;
+});
+
+const liCaracteres = liTodos[1];
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  const numeroCaracteres = analyzer.getCharacterCount(textToAnalyze);
+  liCaracteres.innerHTML = `Caracteres: ${numeroCaracteres}`;
+});
+
+const liCaracteresSinEspacio = liTodos[2];
+console.log(liCaracteresSinEspacio)
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  const numeroCaracteresSinEspacio = analyzer.getCharacterCountExcludingSpaces(textToAnalyze);
+  liCaracteresSinEspacio.innerHTML = `Caracteres sin espacio: ${numeroCaracteresSinEspacio}`
+});
+
+const liNumeros = liTodos[3];
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  const numeros = analyzer.getNumberCount(textToAnalyze);
+  liNumeros.innerHTML = `Numeros: ${numeros}`
+});
+
+const liSumaNumeros = liTodos[4];
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  const sumaNumeros = analyzer.getNumberSum(textToAnalyze);
+  liSumaNumeros.innerHTML = `Suma de numeros: ${sumaNumeros}`
+});
+
+const liLongitud = liTodos[5];
+
+textarea.addEventListener('input', () => {
+  const textToAnalyze = textarea.value;
+  const longitud = analyzer.getAverageWordLength(textToAnalyze);
+  liLongitud.innerHTML = `Longitud promedio: ${longitud}`
+});
+
+const botonLimpiar = document.getElementById('reset-button');
+//limpio todo el contenido de un textarea
+botonLimpiar.addEventListener("click", () => {
+  textarea.value = '';
+  //remplazo los elementos de un li por 0 esto al hacer click en el boton
+
+  liTodos.forEach(li => {
+    li.innerHTML = li.innerHTML.replace(/\d+/, '0');
+    li.innerHTML = li.innerHTML.replace(/[0-9]*\.[0-9]*/g, '0');
+  });
+});
